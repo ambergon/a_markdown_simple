79566f3ad41c9fe0340bb200360823bccc721fa2
ambergon <foxgon@outlook.jp>
2021-01-10 23:53:22 +0900

    [change]
    detailを完全に分離して排除

--- a_markdown_simple.py
+++ a_markdown_simple.py
@@ -26,15 +26,6 @@ import xml.etree.ElementTree as etree
 #!!red|str!! >> <span class="red">spam</span>'
 SPAN_CLASS = r'[!]{2}(?P<class>.+?)[|](?P<text>.+?)[!]{2}'
 
-#{{summary}} >> <summary>summary_title</summary>
-SUMMARY = r'[{]{2}(?P<title>.+?)[}]{2}'
-
-#--div--
-
-#detail
-RE_FENCE_START = r'^{{3}\n'
-RE_FENCE_END = r'}{3}'
-
 class MyExtension(Extension):
     def extendMarkdown(self, md, md_globals):
 
@@ -42,12 +33,6 @@ class MyExtension(Extension):
         span_class=SpanClassPattern(SPAN_CLASS, md)
         md.inlinePatterns['custom_span_class'] = span_class
 
-        #summary
-        summary=SummaryPattern(SUMMARY, md)
-        md.inlinePatterns['custom_summary'] = summary
-    
-        #detail
-        md.parser.blockprocessors.register(DetailBlock(md.parser),'box',175)
 
 class SpanClassPattern(Pattern):
     def handleMatch(self, matched):
@@ -59,35 +44,6 @@ class SpanClassPattern(Pattern):
         line.set("class", cls)
         line.text = markdown.util.AtomicString(text)
         return line
-
-class SummaryPattern(Pattern):
-    def handleMatch(self, matched):
-
-        text = matched.group("title")
-        line = markdown.util.etree.Element("summary")
-        line.text = markdown.util.AtomicString(text)
-        return line
-
-class DetailBlock(BlockProcessor):
-    def test(self,parent,block):
-        return re.match(RE_FENCE_START,block)
-
-    def run(self,parent,blocks):
-        original_block = blocks[0]
-        blocks[0] = re.sub(RE_FENCE_START,'',blocks[0])
-
-        for block_num,block in enumerate(blocks):
-            if re.search(RE_FENCE_END,block):
-                blocks[block_num] = re.sub(RE_FENCE_END,'',block)
-                e = etree.SubElement(parent,'details')
-                #e.set('open','')
-                #e.set('style','display: inline-block; border: 1px solid red;')
-                self.parser.parseBlocks(e,blocks[0:block_num + 1])
-                for i in range(0,block_num + 1):
-                    blocks.pop(0)
-                return True
-        blocks[0] = original_block
-        return False
 
 def makeExtension(*args, **kwargs):
     return MyExtension(*args, **kwargs)
